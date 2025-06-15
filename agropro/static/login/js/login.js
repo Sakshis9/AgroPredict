d5ae09b606c44ae0551a8a7561a19fec72e66ebe
Sakshi <122632893+Sakshis9@users.noreply.github.com>
2025-06-15 22:06:38 +0530

    Add files via upload

---
+++ agropro/static/login/js/login.js
@@ -0,0 +1,58 @@
+$(function() {
+$(".btn").click(function() {
+$(".form-signin").toggleClass("form-signin-left");
+$(".form-signup").toggleClass("form-signup-left");
+$(".frame").toggleClass("frame-long");
+$(".signup-inactive").toggleClass("signup-active");
+$(".signin-active").toggleClass("signin-inactive");
+$(".forgot").toggleClass("forgot-left");
+$(this).removeClass("idle").addClass("active");
+});
+});
+
+$(function() {
+$(".btn-signup").click(function() {
+$(".nav").toggleClass("nav-up");
+$(".form-signup-left").toggleClass("form-signup-down");
+$(".success").toggleClass("success-left");
+$(".frame").toggleClass("frame-short");
+});
+});
+
+$(function() {
+$(".btn-signin").click(function() {
+$(".btn-animate").toggleClass("btn-animate-grow");
+$(".welcome").toggleClass("welcome-left");
+$(".cover-photo").toggleClass("cover-photo-down");
+$(".frame").toggleClass("frame-short");
+$(".profile-photo").toggleClass("profile-photo-down");
+$(".btn-goback").toggleClass("btn-goback-up");
+$(".forgot").toggleClass("forgot-fade");
+});
+});
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$(".toggle-password").click(function() {
+
+	  $(this).toggleClass("fa-eye fa-eye-slash");
+	  var input = $($(this).attr("toggle"));
+	  if (input.attr("type") == "password") {
+	    input.attr("type", "text");
+	  } else {
+	    input.attr("type", "password");
+	  }
+	});
+
+})(jQuery);
